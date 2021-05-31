4309f427c9894944172fd661176ab03b1d9af6d6
wowsive <swasive172172@bscse.uiu.ac.bd>
2021-05-31 16:03:54 +0600

    Updated the file

--- sum_using_forloop.c
+++ sum_using_forloop.c
@@ -1,15 +1,21 @@
 #include<stdio.h>
 int sum(int arr[], int n) {
-// complete this function using for loop to compute the sum of arr
+ int s = 0;
+ int i;
+ for (i=0;i<n;i++){
+ s += arr[i];
+ }
+ return s;
 }
 int main() {
  int arr[100];
  int i,j,k,n;
  printf("Enter size of array ");
- // take array size input
+ scanf("%d",&n);
  for(i=0;i<n;i++)
  {
- // take array elements input
+ printf("%dth element ",i+1);
+ scanf("%d",&arr[i]);
  }
  printf("sum of the elements of this array is %d", sum(arr, n));
  return 0;
