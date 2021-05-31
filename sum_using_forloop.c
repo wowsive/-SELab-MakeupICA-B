6984b744615e18e549b064f9a0b1a4fa2a4ecbc6
wowsive <swasive172172@bscse.uiu.ac.bd>
2021-05-31 15:52:02 +0600

    code commited

---
+++ sum_using_forloop.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int sum(int arr[], int n) {
+// complete this function using for loop to compute the sum of arr
+}
+int main() {
+ int arr[100];
+ int i,j,k,n;
+ printf("Enter size of array ");
+ // take array size input
+ for(i=0;i<n;i++)
+ {
+ // take array elements input
+ }
+ printf("sum of the elements of this array is %d", sum(arr, n));
+ return 0;
+}
